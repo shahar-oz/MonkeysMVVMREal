1e18332a8ccc0cf64e440373f8883a3355c2d88a
User <User@LAB3-3>
2024-02-12 15:39:57 +0200

    Add project files.

---
+++ ViewModels/MonkeyPageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using MonkeysMVVM.Models;
+using MonkeysMVVM.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Google.Crypto.Tink.Signature;
+
+namespace MonkeysMVVM.ViewModels
+{
+    public class MonkeyPageViewModel:ViewModel
+    {
+        public ObservableCollection<Monkey> Monkeys { get; set; }
+        public ICommand LoadMonkeysCommand { get; private set; }
+
+        public MonkeyPageViewModel()
+        {
+            Monkeys = new ObservableCollection<Monkey>();
+
+            LoadMonkeysCommand = new Command(async () => await LoadMonkeys());
+        }
+
+        private Task LoadMonkeys()
+        {
+            MonkeysService monkeys = new MonkeysService();
+            var list = monkeys.GetMonkey();
+            for(int i =0; i < list.Count; i++)
+            {
+                Monkeys.Add(list[i]);
+            }
+            return Task.CompletedTask;
+         }
+    }
+}
